99505cf1903b805646a0beb99b05f2443ce59139
satori-do <volodymyrreznich@gmail.com>
2019-09-06 16:04:34 +0300

    Minimal LAMP-draft.

---
+++ php/index.php
@@ -0,0 +1,14 @@
+<!-- ./php/index.php -->
+
+<html>
+    <head>
+        <tiile>Hello World</title>
+    </head>
+
+    <body>
+        <?php
+            echo "Hello, World!"
+        ?>
+        <?php phpinfo(); ?>
+    </body>
+</html>
