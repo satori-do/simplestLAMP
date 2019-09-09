44d623c74d3bdec15e000ad8408ac2979beeb03b
satori-do <volodymyrreznich@gmail.com>
2019-09-09 15:03:54 +0300

    Was added db connector checker with build of modules.

--- php/index.php
+++ php/index.php
@@ -2,13 +2,30 @@
 
 <html>
     <head>
-        <tiile>Hello World</title>
+        <tiile>Base Page</title>
+        <br />
+        <br />
     </head>
 
     <body>
         <?php
             echo "Hello, World!"
         ?>
-        <?php phpinfo(); ?>
+        <?php
+         $dbhost = "172.18.0.2";
+         $dbuser = "devuser";
+         $dbpass = "devpass";
+         $db = "test_db";
+         // Create connection
+          $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$db);
+          // Check connection
+          if (!$conn) {
+             die("Connection failed: " . mysqli_connect_error());
+          }
+          echo "Connected successfully";
+          ?>
+          <?php
+              phpinfo();
+          ?>
     </body>
 </html>
